4ac2c74c2324b51ee340ee50fd70a597645ff83b
Mino1157 <121426824+Mino1157@users.noreply.github.com>
2023-05-15 12:25:37 +0900

    .

--- src/main/webapp/js/member/modify.js
+++ src/main/webapp/js/member/modify.js
@@ -13,3 +13,29 @@ $("#inputPassword,#inputPasswordCheck").keyup(function() {
 		$("#passwordSuccessText").addClass("d-none")
 	}
 })
+
+
+$("#checkNickNameBtn").click(function() {
+	const userId = $("#inputId").val();
+	const userNickName = $("#inputNickName").val();
+	$.ajax("/member/checkNickname/" + userNickName, {
+		success: function(data) {
+			if (data.available) {
+				$("#availableNicknameMessage").removeClass("d-none");
+				$("#notAvailableNickNameMessage").addClass("d-none");
+				checkNickName = true;
+			} else {
+				if (data.id == userId) {
+					$("#availableNicknameMessage").removeClass("d-none");
+					$("#notAvailableNickNameMessage").addClass("d-none");
+					checkNickName = true;
+				} else{
+					$("#availableNicknameMessage").addClass("d-none");
+					$("#notAvailableNickNameMessage").removeClass("d-none");
+					checkNickName = false;
+					
+				}
+			}
+		}
+	})
+})
